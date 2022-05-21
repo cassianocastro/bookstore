b9f901e1fc7247b0314f3effb638c28af1ea517b
cassianocastro <tottenham248@gmail.com>
2022-05-21 19:16:31 -0300

    Add file.

---
+++ src/model/entities/Entity.java
@@ -0,0 +1,21 @@
+package model.entities;
+
+/**
+ *
+ *
+ */
+abstract public class Entity
+{
+
+    private final int id;
+
+    public Entity(int id)
+    {
+        this.id = id;
+    }
+
+    public int getID()
+    {
+        return this.id;
+    }
+}
