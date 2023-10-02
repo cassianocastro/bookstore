b2a69940f38d0cdc2f42a95125458bb938c54849
Cassiano Castro <development.castro@gmail.com>
2023-10-02 16:33:24 -0300

    Class creation

--- src/model/utils/Phone.java
+++ src/model/utils/Phone.java
@@ -3,7 +3,31 @@ package model.utils;
 /**
  *
  */
-public class Product_1
+public class Phone
 {
 
+    private final int ddd;
+    private final int number;
+    
+    public Phone(int ddd, int number)
+    {
+        this.ddd    = ddd;
+        this.number = number;
+    }
+    
+    public int getDDD()
+    {
+        return this.ddd;
+    }
+    
+    public int getNumber()
+    {
+        return this.number;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "(" + this.ddd + ") " + this.number;
+    }
 }
